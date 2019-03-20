dbaf6b4a5d1c7320922ac29381c319eae63bc0aa
varusan <subaru.nakamura@drecom.co.jp>
2019-03-20 17:02:43 +0900

    [from now] 2019/03/20 17:02:43
    diff --git a/cloudbuild.yaml b/cloudbuild.yaml
    index aafb2ef..31e636b 100644
    --- a/cloudbuild.yaml
    +++ b/cloudbuild.yaml
    @@ -1,21 +1,21 @@
     steps:
    -- name: gcr.io/kaniko-project/executor
    -  args:
    -  - --destination=gcr.io/$PROJECT_ID/$REPO_NAME:$COMMIT_SHA
    -  - --cache=true
    -
    -- name: 'gcr.io/cloud-builders/docker'
    -  args:
    -  - pull
    -  - gcr.io/$PROJECT_ID/$REPO_NAME:$COMMIT_SHA
    -
    -- name: 'gcr.io/gcp-runtimes/container-structure-test'
    -  args:
    -  - test
    -  - --image
    -  - gcr.io/$PROJECT_ID/$REPO_NAME:$COMMIT_SHA
    -  - --config
    -  - container-structure-test.yml
    +# - name: gcr.io/kaniko-project/executor
    +#   args:
    +#   - --destination=gcr.io/$PROJECT_ID/$REPO_NAME:$COMMIT_SHA
    +#   - --cache=true
    +#
    +# - name: 'gcr.io/cloud-builders/docker'
    +#   args:
    +#   - pull
    +#   - gcr.io/$PROJECT_ID/$REPO_NAME:$COMMIT_SHA
    +#
    +# - name: 'gcr.io/gcp-runtimes/container-structure-test'
    +#   args:
    +#   - test
    +#   - --image
    +#   - gcr.io/$PROJECT_ID/$REPO_NAME:$COMMIT_SHA
    +#   - --config
    +#   - container-structure-test.yml
    
     - name: 'gcr.io/cloud-builders/docker'
       secretEnv:
    diff --git a/scripts/push_dockerhub.sh b/scripts/push_dockerhub.sh
    index 5ad7960..888e69e 100755
    --- a/scripts/push_dockerhub.sh
    +++ b/scripts/push_dockerhub.sh
    @@ -1,11 +1,17 @@
     #!/bin/bash
    +cd `dirname $0`/..
    
    -if [ $BRANCH_NAME -eq "master" ]; then
    -  $VERSION="latest"
    +set -e
    +set -x
    +
    +if [ ${BRANCH_NAME} = "master" ]; then
    +  ${VERSION}="latest"
     else
    -  $VERSION=$BRANCH_NAME
    +  ${VERSION}=${BRANCH_NAME}
     fi
    
    +IMAGE_NAME=gcr.io/${PROJECT_ID}/${REPO_NAME}:${COMMIT_SHA}
    +
     docker login --username=drecomdockerhub --password=$$DOCKERHUB_PASSWORD
    -docker tag gcr.io/$PROJECT_ID/$REPO_NAME:$COMMIT_SHA drecom/centos-ruby:$VERSION
    -docker push drecom/centos-ruby:$VERSION
    +docker tag ${IMAGE_NAME} drecom/centos-ruby:${VERSION}
    +docker push drecom/centos-ruby:${VERSION}

--- scripts/push_dockerhub.sh
+++ scripts/push_dockerhub.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+cd `dirname $0`/..
 
-if [ $BRANCH_NAME -eq "master" ]; then
-  $VERSION="latest"
+set -e
+set -x
+
+if [ ${BRANCH_NAME} = "master" ]; then
+  ${VERSION}="latest"
 else
-  $VERSION=$BRANCH_NAME
+  ${VERSION}=${BRANCH_NAME}
 fi
 
+IMAGE_NAME=gcr.io/${PROJECT_ID}/${REPO_NAME}:${COMMIT_SHA}
+
 docker login --username=drecomdockerhub --password=$$DOCKERHUB_PASSWORD
-docker tag gcr.io/$PROJECT_ID/$REPO_NAME:$COMMIT_SHA drecom/centos-ruby:$VERSION
-docker push drecom/centos-ruby:$VERSION
+docker tag ${IMAGE_NAME} drecom/centos-ruby:${VERSION}
+docker push drecom/centos-ruby:${VERSION}
